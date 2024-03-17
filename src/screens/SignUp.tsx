27621a16517a147cd80d0be2dad074f6d733c80f
Mateus Adamo <adamomateus478@gmail.com>
2024-03-17 16:05:11 -0300

    feat: passwords validations create.

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -20,7 +20,9 @@ type FormDataProps = {
 
 const signUpSchema = yup.object({
     name: yup.string().required('Informe o nome'),
-    email: yup.string().required('Informe o e-mail').email('E-mail inválido')
+    email: yup.string().required('Informe o e-mail').email('E-mail inválido'),
+    password: yup.string().required('Informe a senha').min(6, 'A senha deve conter pelo menos 6 dígitos.'),
+    confirmPassword: yup.string().required('Confirme a senha.').oneOf([yup.ref('password'), null], 'A confirmação da senha não confere')
 })
 
 export function SignUp() {
